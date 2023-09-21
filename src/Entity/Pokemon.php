c0beb2deddef2e7ee4aaaec06e93f04aed4cb182
Amelie Le Coz <lecozamelie@hotmail.com>
2023-09-21 14:54:16 +0200

    fix pagination on index

--- src/Entity/Pokemon.php
+++ src/Entity/Pokemon.php
@@ -9,7 +9,10 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PokemonRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    paginationClientItemsPerPage: true,
+    paginationItemsPerPage: 50,
+)]
 class Pokemon
 {
     #[ORM\Id]
